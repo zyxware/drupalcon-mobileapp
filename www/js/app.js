e37504cb776f35996bd2e262ccd2bbfb5cd2a98b
Nisam <nisamudheen.k@zyxware.com>
2016-03-28 12:31:09 +0530

    auto data insert is configured

--- www/js/app.js
+++ www/js/app.js
@@ -6,96 +6,96 @@
 // 'starter.controllers' is found in controllers.js
 var db = null;
 
-angular.module('starter', ['ionic', 'starter.controllers','ngStorage','ngCordova','starter.config'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngStorage', 'ngCordova', 'starter.config'])
 
-.run(function($ionicPlatform,$cordovaSQLite, DB_CONFIG) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform, $cordovaSQLite, DB_CONFIG) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-    // db = $cordovaSQLite.openDB({ name: "my.db" });
-    if (window.cordova) {
-      db = $cordovaSQLite.openDB({ name: DB_CONFIG.name }); //device
-    }else{
-      db = window.openDatabase(DB_CONFIG.name, '1', 'd_conference', 1024 * 1024 * 100); // browser
-    }
-
-
-          angular.forEach(DB_CONFIG.tables, function(table) {
-              var columns = [];
-
-              angular.forEach(table.columns, function(column) {
-                  columns.push(column.name + ' ' + column.type);
-              });
-
-              var query = 'CREATE TABLE IF NOT EXISTS ' + table.name + ' (' + columns.join(',') + ')';
-              $cordovaSQLite.execute(db, query);
-          });
-
-  });
-})
-    .factory('readJson', function ($http) {
-        return {
-            get: function () {        
-                 return  $http.get('json/DrupalCon_JsonData_v1.json');
-
-            }
-        }
-    })
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.sessions', {
-    url: '/sessions',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/sessions.html',
-        controller: 'JsonController',
       }
-    }
-  })
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+      // db = $cordovaSQLite.openDB({ name: "my.db" });
+      if (window.cordova) {
+        db = $cordovaSQLite.openDB({name: DB_CONFIG.name}); //device
+      } else {
+        db = window.openDatabase(DB_CONFIG.name, '1', 'd_conference', 1024 * 1024 * 100); // browser
+      }
 
-  .state('app.speakers', {
-      url: '/speakers',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/speakers.html'
-        }
-      }
-    })
-  // .state('app.sessiondetails', {
-  //     url: '/sessiondetails',
-  //     views: {
-  //       'menuContent': {
-  //         templateUrl: 'templates/sessiondetails.html',
-  //         controller: 'sessiondetails',
-  //       }
-  //     }
-  //   })
-    .state('app.tracks', {
-      url: '/tracks',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/tracks.html',
-        }
-      }
+
+      angular.forEach(DB_CONFIG.tables, function (table) {
+        var columns = [];
+
+        angular.forEach(table.columns, function (column) {
+          columns.push(column.name + ' ' + column.type);
+        });
+        $cordovaSQLite.execute(db, 'DROP TABLE programs');
+        var query = 'CREATE TABLE IF NOT EXISTS ' + table.name + ' (' + columns.join(',') + ')';
+        $cordovaSQLite.execute(db, query);
+      });
+
     });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/sessions');
-});
+  })
+  .factory('readJson', function ($http) {
+    return {
+      get: function () {
+        return $http.get('json/DrupalCon_JsonData_v1.json');
+
+      }
+    }
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.sessions', {
+        url: '/sessions',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/sessions.html',
+            controller: 'ProgramCtrl',
+          }
+        }
+      })
+
+      .state('app.speakers', {
+        url: '/speakers',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/speakers.html'
+          }
+        }
+      })
+      // .state('app.sessiondetails', {
+      //     url: '/sessiondetails',
+      //     views: {
+      //       'menuContent': {
+      //         templateUrl: 'templates/sessiondetails.html',
+      //         controller: 'sessiondetails',
+      //       }
+      //     }
+      //   })
+      .state('app.tracks', {
+        url: '/tracks',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/tracks.html',
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/sessions');
+  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,37 +1,69 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-  
-})
- .controller('JsonController', function ($scope, readJson) {
 
- 	 $scope.insert = function(firstname, lastname) {
-        var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
-        $cordovaSQLite.execute(db, query, [firstname, lastname]).then(function(res) {
-            console.log("INSERT ID -> " + res.insertId);
-        }, function (err) {
-            console.error(err);
+  })
+  .controller('ProgramCtrl', function ($scope, readJson, DB_CONFIG,$cordovaSQLite) {
+    window.localStorage.setItem('db-initialized', 'null');
+    if (window.localStorage.getItem('db-initialized') == 'null') {
+      console.log(window.localStorage.getItem('db-initialized'));
+
+      readJson.get().success(function (jsonData) {
+
+        angular.forEach(DB_CONFIG.tables, function (table) {
+
+          angular.forEach(jsonData[table.name], function(tableData){
+
+            //console.log(jsonData[table.name]);
+
+            var columns = [];
+            var params = [];
+            var fieldValues = [];
+
+
+            angular.forEach(table.columns, function (column) {
+              if(column.name != 'id' ) {
+                columns.push(column.name);
+                params.push('?');
+                if(column.name != 'id' )
+                  fieldValues.push(tableData[column.name]);
+              }
+
+
+            });
+
+            var query = 'INSERT INTO ' + table.name + ' (' + columns.join(',') + ') VALUES (' + params.join(',') + ')';
+
+            $cordovaSQLite.execute(db, query, fieldValues).then(function (res) {
+              console.log("INSERT ID -> " + res.insertId);
+            }, function (err) {
+              console.error(err);
+            });
+
+          });
+
         });
+
+      });
+      window.localStorage.setItem('db-initialized', 1);
     }
 
-    $scope.select = function(lastname) {
-        var query = "SELECT firstname, lastname FROM people WHERE lastname = ?";
+    //$scope.select = function (lastname) {
+    //  var query = "SELECT firstname, lastname FROM people WHERE lastname = ?";
+    //
+    //  $cordovaSQLite.execute(db, query, [lastname]).then(function (res) {
+    //    if (res.rows.length > 0) {
+    //      console.log("SELECTED -> " + res.rows.item(0).firstname + " " + res.rows.item(0).lastname);
+    //    } else {
+    //      console.log("No results found");
+    //    }
+    //  }, function (err) {
+    //    console.error(err);
+    //  });
+    //}
 
-        $cordovaSQLite.execute(db, query, [lastname]).then(function(res) {
-            if(res.rows.length > 0) {
-                console.log("SELECTED -> " + res.rows.item(0).firstname + " " + res.rows.item(0).lastname);
-            } else {
-                console.log("No results found");
-            }
-        }, function (err) {
-            console.error(err);
-        });
-    }
-        readJson.get().success(function (msg) {
-            $scope.jsondata = msg;
-        });
-    });
+  });
 
 // .controller('JsonController', function($scope, $stateParams) {
 //     console.log('hello');
