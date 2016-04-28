1567027e48214dc3da4f75f5620cb4ebf8cf9236
Dhanya Girish <dhanya.girish@zyxware.com>
2016-04-28 15:05:15 +0530

    Implements downloading updated JSON file from the server.
    Downloads the updated input JSON file from the server, and updates the app data according to the new JSON file.
    Brings modularity to the app, by adding all the factories to the services.js file.

--- www/js/app.js
+++ www/js/app.js
@@ -26,322 +26,91 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngStorage', 'ngCordo
         db = window.openDatabase(DB_CONFIG.name, '1', 'd_conference', 1024 * 1024 * 100); // browser
       }
       var currentJsonVersion = window.localStorage.getItem('json-version');
-      console.log("currentJsonVersion" + currentJsonVersion);
-      // Temporarily commenting out the version checking code.
-      //window.localStorage.setItem('json-version', 1);
-      var params = headers = [];
-      var isOnline = $cordovaNetwork.isOnline();
-      console.log(isOnline);
-      // listen for Online event - on device
-      //$rootScope.$on('$cordovaNetwork:online', function(event, networkState){
-      if(isOnline == true) {
-        console.log('inside online');
-        ajaxService.ajax('json-version', params, headers).then(function (response) {
-          currentJsonVersion = response.data.version;
-          if(window.localStorage.getItem('json-version') < currentJsonVersion) {
-            var confirmPopup = $ionicPopup.confirm({
-              title: 'Update Available',
-              template: 'An update conference is available. Do you want to update the data?'
-            });
-            confirmPopup.then(function(res) {
-              if(res) {
-                dataService.getJsonFile().then(function (response) {
-                  console.log('response' + response);
-                  if(window.localStorage.getItem('json-version') < currentJsonVersion) {
-                    window.localStorage.setItem('db-initialized', 0);
-                    angular.forEach(DB_CONFIG.tables, function (table) {
-                      if (table.name != 'bookmarks') {
-                        var query = 'DROP TABLE ' + table.name;
-                        $cordovaSQLite.execute(db, query);
-                      }
-                    });
-                  }
-                })
-              } else {
-                console.log('No need to update');
-              }
-            })
-          }  
-        });
-      }
-      // listen for Online event - on browser
-      window.addEventListener("online", function(e) {
-        ajaxService.ajax('json-version', params, headers).then(function (response) {
-          currentJsonVersion = response.data.version;
-          if(window.localStorage.getItem('json-version') < currentJsonVersion) {
-            var confirmPopup = $ionicPopup.confirm({
-              title: 'Update Available',
-              template: 'An update conference is available. Do you want to update the data?'
-            });
-            confirmPopup.then(function(res) {
-              if(res) {
-                dataService.getJsonFile().then(function (response) {
-                  console.log('response' + response);
-                  if(window.localStorage.getItem('json-version') < currentJsonVersion) {
-                    window.localStorage.setItem('db-initialized', 0);
-                    angular.forEach(DB_CONFIG.tables, function (table) {
-                      if (table.name != 'bookmarks') {
-                        var query = 'DROP TABLE ' + table.name;
-                        $cordovaSQLite.execute(db, query);
-                      }
-                    });
-                  }
-                })
-              } else {
-                console.log('No need to update');
-              }
-            })
-          }  
-        });
-      }, true);
-      /*angular.forEach(DB_CONFIG.tables, function (table) {
-                    if (table.name != 'bookmarks') {
-                      var query = 'DROP TABLE ' + table.name;
-                      $cordovaSQLite.execute(db, query);
-                    }
-                  });
-      window.localStorage.setItem('db-initialized', 0);*/
       if (window.localStorage.getItem('db-initialized') != 1) {
-        angular.forEach(DB_CONFIG.tables, function (table) {
-          var columns = [];
-          angular.forEach(table.columns, function (column) {
-            columns.push(column.name + ' ' + column.type);
-          });
-          var query = 'CREATE TABLE IF NOT EXISTS ' + table.name + ' (' + columns.join(',') + ')';
-          $cordovaSQLite.execute(db, query);
-        });
-        var url = "";
+        // Intializing the db for first-time.
+        console.log('inside db-initlization');
         if(ionic.Platform.isAndroid()){
-          url = "/android_asset/www/";
+          url = "/android_asset/www/json/sessions.json";
         }
-        $http.get(url+'json/sessions.json').success(function (jsonData) {
-          /*$ionicLoading.show({
-            template: 'Intializing...'
-          });*/
-          console.log(jsonData['version']);
-          window.localStorage.setItem('json-version', jsonData['version']);
-          angular.forEach(DB_CONFIG.tables, function (table) {
-            angular.forEach(jsonData[table.name], function (tableData) {
-              var columns = [];
-              var params = [];
-              var fieldValues = [];
-              angular.forEach(table.columns, function (column) {
-                  columns.push(column.name);
-                  params.push('?');
-                  fieldValues.push(tableData[column.name]);
-              });
-
-              var query = 'INSERT INTO ' + table.name + ' (' + columns.join(',') + ') VALUES (' + params.join(',') + ')';
-              $cordovaSQLite.execute(db, query, fieldValues).then(function (res) {
-                console.log("Data Inserted into " + table.name);
-              }, function (err) {
-                console.error(err);
-              });
-            });
-          });
-        });
-        window.localStorage.setItem('db-initialized', 1);
+        updateDB(url);
       }
-      $ionicLoading.hide();
+      else {
+        //updating the db a/c updations in the website.
+        var params = headers = [];
+        // listen for Online event - on device
+        var isOnline = $cordovaNetwork.isOnline();
+        if(isOnline == true) {
+          ajaxService.ajax('json-version', params, headers).then(function (response) {
+            currentJsonVersion = response.data.version;
+            if(window.localStorage.getItem('json-version') < currentJsonVersion) {
+              var confirmPopup = $ionicPopup.confirm({
+                title: 'Update Available',
+                template: 'An update for Confernece schedule is available. Do you want to update the data?'
+              });
+              confirmPopup.then(function(res) {
+                if(res) {
+                  dataService.getJsonFile().then(function (response) {
+                    var url = response.nativeURL;
+                    if(window.localStorage.getItem('json-version') < currentJsonVersion) {
+                      console.log("inside getJson Success callback.");
+                      angular.forEach(DB_CONFIG.tables, function (table) {
+                        if (table.name != 'bookmarks') {
+                          var query = 'DROP TABLE ' + table.name;
+                          $cordovaSQLite.execute(db, query);
+                        }
+                      });
+                      updateDB(url);
+                    }
+                  })
+                } else {
+                  console.log('No need to update');
+                }
+              })
+            }  
+          });
+        }
+      }
     });
-  })
-  .factory('sessionService', function($q, $cordovaSQLite, $filter) {
-    return {
-       //factory to get the session details based on the filters.
-      getSessions: function(filterKey, filterValue) {
-        var q = $q.defer();
-        var result = [];
-        var query = "SELECT programs.id, programs.title, programs.date, programs.startTime, programs.endTime,  ";
-        query += "tracks.title AS tracktitle, ";
-        query += "rooms.name AS roomname, rooms.id AS roomid ";
-        query += "FROM programs ";
-        query += "JOIN tracks ON tracks.id = programs.track ";
-        query += "JOIN rooms ON rooms.id = programs.room ";
-        query += "JOIN sessionSpeakers ON sessionSpeakers.sessionId = programs.id ";
-        query += "JOIN speakers ON speakers.id = sessionSpeakers.speakerId ";
-        // if view-pastevents = 0, view future events only, else view past events also.
-        if (window.localStorage.getItem('view-pastevents') == 0) {
-          query += "WHERE programs.date > date('now') ";
-        }
-        else {
-          query += "WHERE 1";
-        }
-        if( filterKey == 'date') {
-          var filterValue = $filter('date')(filterValue, "yyyy-MM-dd HH:mm:ss");
-          query += " AND programs.date = ?";
-        }
-        if( filterKey == 'room') {
-          query += " AND programs.room = ?";
-        }
-        if( filterKey == 'track') {
-          query += " AND programs.track = ?";
-        }
-        if( filterKey == 'speaker') {
-          query += " AND sessionSpeakers.speakerId = ?";
-        }
-        query += " GROUP BY programs.id";
-        query += " ORDER BY programs.date";
-       $cordovaSQLite.execute(db, query, [filterValue]).then(function (res) {
-          if (res.rows.length > 0) {
-            for (var i = 0; i < res.rows.length; i++) {
-              result.push(res.rows.item(i));
-            }
-            q.resolve(result);
-          } else {
-            //console.log("No results found");
-          }
-        }, function (err) {
-           q.reject(null);
+    /**
+     * Function to create the db enteries. 
+     * 
+     * @param url - path to the json file from which db is to be intilized.
+     */
+    var updateDB = function(url){
+      $ionicLoading.show({
+        template: '<div align="center" ><ion-spinner style="stroke:#3577E8!important"  icon="android"></ion-spinner></div>'
+      });
+      angular.forEach(DB_CONFIG.tables, function (table) {
+        var columns = [];
+        angular.forEach(table.columns, function (column) {
+          columns.push(column.name + ' ' + column.type);
         });
-        return q.promise;
-      },
-      // Get the bookmarked sessions and speakers based on the type filter.
-      getFavourites: function(type) {
-        var q = $q.defer();
-        var result = [];
-        var select = "SELECT bookmarks.id, bookmarks.type, bookmarks.itemId ";
-        var from = "FROM bookmarks ";
-        var where = "WHERE bookmarks.type = ?";
-        var join = groupby = orderby = "";
-        if (type == 'session') {
-          select += ", programs.id, programs.title, programs.date, programs.startTime, programs.endTime, ";
-          select += "rooms.name AS roomname, rooms.id AS roomid ";
-          join += "JOIN programs ON programs.id = bookmarks.itemId ";
-          join += "JOIN rooms ON rooms.id = programs.room ";
-          //Checking past events filter.
-          if(window.localStorage.getItem('view-pastevents') == 0) {
-            where += " AND programs.date > date('now') ";
-          }
-          grouby = " GROUP BY programs.id";
-          orderby = " ORDER BY programs.date";
-        } 
-        else if(type == 'speaker') {
-          select += ", speakers.id, speakers.name, speakers.desgn ";
-          join += "JOIN speakers ON speakers.id = bookmarks.itemId ";
-        }
-        var query = select + from + join + where + groupby + orderby;
-        $cordovaSQLite.execute(db, query, [type]).then(function (res) {
-          if (res.rows.length > 0) {
-            for (var i = 0; i < res.rows.length; i++) {
-              result.push(res.rows.item(i));
-            }
-            q.resolve(result);
-          } else {
-            //console.log("No results found");
-          }
-        }, function (err) {
-          q.reject(null);
-        });
-        return q.promise;
-      },
-      // Get the tracks. If id is provided return the details of individual track.
-      getTracks: function(id) {
-        var q = $q.defer();
-        var result = [];
-        var query = "SELECT tracks.id, tracks.title AS name FROM tracks";
-        if(id != null){
-          query += " WHERE id = " + id; 
-        }
-        else {
-          query += " WHERE 1";
-        }  
-        $cordovaSQLite.execute(db, query).then(function (res) {
-          if (res.rows.length > 0) {
-            for (var i = 0; i < res.rows.length; i++) {
-              result.push(res.rows.item(i));
-            }
-            q.resolve(result);
-          } else {
-            console.log("No results found");
-          }
-        }, function (err) {
-          q.reject(null);
-        });
-        return q.promise;
-      },
-      // Get the rooms. If id is provided return the details of individual room.
-      getRooms: function(id) {
-        var q = $q.defer();
-        var result = [];
-        var query = "SELECT id, name FROM rooms";
-        if(id != null){
-          query += " WHERE id = " + id; 
-        }
-        else {
-          query += " WHERE 1";
-        }  
-        $cordovaSQLite.execute(db, query).then(function (res) {
-          if (res.rows.length > 0) {
-            for (var i = 0; i < res.rows.length; i++) {
-              result.push(res.rows.item(i));
-            }
-            q.resolve(result);
-          } else {
-            console.log("No results found");
-          }
-        }, function (err) {
-          q.reject(null);
-        });
-        return q.promise;
-      },
-      // Get the speakers. If id is provided return the details of individual speaker.
-      getSpeakers: function(id) {
-        var q = $q.defer();
-        var result = [];
-        var query = "SELECT speakers.id, speakers.name, speakers.desc, speakers.desgn FROM speakers";
-        if(id != null){
-          query += " WHERE id = " + id; 
-        }
-        else {
-          query += " WHERE 1";
-        }  
-        $cordovaSQLite.execute(db, query).then(function (res) {
-          if (res.rows.length > 0) {
-            for (var i = 0; i < res.rows.length; i++) {
-              result.push(res.rows.item(i));
-            }
-            q.resolve(result);
-          } else {
-            console.log("No results found");
-          }
-        }, function (err) {
-          q.reject(null);
-        });
-        return q.promise;
-      }
-    }
-  })
-  
-  .factory('dataService', function($q, $cordovaFileTransfer) {
-    return {
-      getJsonFile: function () {
-        var deferred = $q.defer();
-        var url = "http://dev.drupalcon.z9.dev.zyxware.net/sites/default/files/sessions.json";
-        var targetPath = "";
-        if(ionic.Platform.isAndroid()){
-          targetPath = "file:///android_asset/www/";
-        }
-        targetPath = targetPath + "json/sessions.json";
-        console.log(targetPath);
-        var trustHosts = true;
-        var options = {};
+        var query = 'CREATE TABLE IF NOT EXISTS ' + table.name + ' (' + columns.join(',') + ')';
+        $cordovaSQLite.execute(db, query);
+      });
+      $http.get(url).success(function (jsonData) {
+        window.localStorage.setItem('json-version', jsonData['version']);
+        angular.forEach(DB_CONFIG.tables, function (table) {
+          angular.forEach(jsonData[table.name], function (tableData) {
+            var columns = [];
+            var params = [];
+            var fieldValues = [];
+            angular.forEach(table.columns, function (column) {
+                columns.push(column.name);
+                params.push('?');
+                fieldValues.push(tableData[column.name]);
+            });
 
-        $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
-          .then(function(result) {
-            conosle.log(result);
-            console.log('success');
-            deferred.resolve(result);
-          }, function(err) {
-            console.log('Error' + JSON.stringify(err, null, 2));
-            deferred.reject(err);
-          }, function (progress) {
-            $timeout(function () {
-              $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+            var query = 'INSERT INTO ' + table.name + ' (' + columns.join(',') + ') VALUES (' + params.join(',') + ')';
+            $cordovaSQLite.execute(db, query, fieldValues).then(function (res) {
+            }, function (err) {
+              console.error(err);
             });
           });
-          console.log('deferred.promise' + deferred.promise);
-          return deferred.promise;
-      }
+        });
+      });
+      window.localStorage.setItem('db-initialized', 1);
+      $ionicLoading.hide();
     }
   })
   .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
--- www/js/services.js
+++ www/js/services.js
@@ -24,4 +24,191 @@ angular.module('starter.services', [])
       });
     }
   }
+})
+.factory('sessionService', function($q, $cordovaSQLite, $filter) {
+  return {
+     //factory to get the session details based on the filters.
+    getSessions: function(filterKey, filterValue) {
+      var q = $q.defer();
+      var result = [];
+      var query = "SELECT programs.id, programs.title, programs.date, programs.startTime, programs.endTime,  ";
+      query += "tracks.title AS tracktitle, ";
+      query += "rooms.name AS roomname, rooms.id AS roomid ";
+      query += "FROM programs ";
+      query += "JOIN tracks ON tracks.id = programs.track ";
+      query += "JOIN rooms ON rooms.id = programs.room ";
+      query += "JOIN sessionSpeakers ON sessionSpeakers.sessionId = programs.id ";
+      query += "JOIN speakers ON speakers.id = sessionSpeakers.speakerId ";
+      // if view-pastevents = 0, view future events only, else view past events also.
+      if (window.localStorage.getItem('view-pastevents') == 0) {
+        query += "WHERE programs.date > date('now') ";
+      }
+      else {
+        query += "WHERE 1";
+      }
+      if( filterKey == 'date') {
+        var filterValue = $filter('date')(filterValue, "yyyy-MM-dd HH:mm:ss");
+        query += " AND programs.date = ?";
+      }
+      if( filterKey == 'room') {
+        query += " AND programs.room = ?";
+      }
+      if( filterKey == 'track') {
+        query += " AND programs.track = ?";
+      }
+      if( filterKey == 'speaker') {
+        query += " AND sessionSpeakers.speakerId = ?";
+      }
+      query += " GROUP BY programs.id";
+      query += " ORDER BY programs.date";
+     $cordovaSQLite.execute(db, query, [filterValue]).then(function (res) {
+        if (res.rows.length > 0) {
+          for (var i = 0; i < res.rows.length; i++) {
+            result.push(res.rows.item(i));
+          }
+          q.resolve(result);
+        } else {
+          //console.log("No results found");
+        }
+      }, function (err) {
+         q.reject(null);
+      });
+      return q.promise;
+    },
+    // Get the bookmarked sessions and speakers based on the type filter.
+    getFavourites: function(type) {
+      var q = $q.defer();
+      var result = [];
+      var select = "SELECT bookmarks.id, bookmarks.type, bookmarks.itemId ";
+      var from = "FROM bookmarks ";
+      var where = "WHERE bookmarks.type = ?";
+      var join = groupby = orderby = "";
+      if (type == 'session') {
+        select += ", programs.id, programs.title, programs.date, programs.startTime, programs.endTime, ";
+        select += "rooms.name AS roomname, rooms.id AS roomid ";
+        join += "JOIN programs ON programs.id = bookmarks.itemId ";
+        join += "JOIN rooms ON rooms.id = programs.room ";
+        //Checking past events filter.
+        if(window.localStorage.getItem('view-pastevents') == 0) {
+          where += " AND programs.date > date('now') ";
+        }
+        grouby = " GROUP BY programs.id";
+        orderby = " ORDER BY programs.date";
+      } 
+      else if(type == 'speaker') {
+        select += ", speakers.id, speakers.name, speakers.desgn ";
+        join += "JOIN speakers ON speakers.id = bookmarks.itemId ";
+      }
+      var query = select + from + join + where + groupby + orderby;
+      $cordovaSQLite.execute(db, query, [type]).then(function (res) {
+        if (res.rows.length > 0) {
+          for (var i = 0; i < res.rows.length; i++) {
+            result.push(res.rows.item(i));
+          }
+          q.resolve(result);
+        } else {
+          //console.log("No results found");
+        }
+      }, function (err) {
+        q.reject(null);
+      });
+      return q.promise;
+    },
+    // Get the tracks. If id is provided return the details of individual track.
+    getTracks: function(id) {
+      var q = $q.defer();
+      var result = [];
+      var query = "SELECT tracks.id, tracks.title AS name FROM tracks";
+      if(id != null){
+        query += " WHERE id = " + id; 
+      }
+      else {
+        query += " WHERE 1";
+      }  
+      $cordovaSQLite.execute(db, query).then(function (res) {
+        if (res.rows.length > 0) {
+          for (var i = 0; i < res.rows.length; i++) {
+            result.push(res.rows.item(i));
+          }
+          q.resolve(result);
+        } else {
+          console.log("No results found");
+        }
+      }, function (err) {
+        q.reject(null);
+      });
+      return q.promise;
+    },
+    // Get the rooms. If id is provided return the details of individual room.
+    getRooms: function(id) {
+      var q = $q.defer();
+      var result = [];
+      var query = "SELECT id, name FROM rooms";
+      if(id != null){
+        query += " WHERE id = " + id; 
+      }
+      else {
+        query += " WHERE 1";
+      }  
+      $cordovaSQLite.execute(db, query).then(function (res) {
+        if (res.rows.length > 0) {
+          for (var i = 0; i < res.rows.length; i++) {
+            result.push(res.rows.item(i));
+          }
+          q.resolve(result);
+        } else {
+          console.log("No results found");
+        }
+      }, function (err) {
+        q.reject(null);
+      });
+      return q.promise;
+    },
+    // Get the speakers. If id is provided return the details of individual speaker.
+    getSpeakers: function(id) {
+      var q = $q.defer();
+      var result = [];
+      var query = "SELECT speakers.id, speakers.name, speakers.desgn FROM speakers";
+      if(id != null){
+        query += " WHERE id = " + id; 
+      }
+      else {
+        query += " WHERE 1";
+      }  
+      $cordovaSQLite.execute(db, query).then(function (res) {
+        if (res.rows.length > 0) {
+          for (var i = 0; i < res.rows.length; i++) {
+            result.push(res.rows.item(i));
+          }
+          q.resolve(result);
+        } else {
+          console.log("No results found");
+        }
+      }, function (err) {
+        q.reject(null);
+      });
+      return q.promise;
+    }
+  }
+})
+
+.factory('dataService', function($q, $cordovaFileTransfer) {
+  return {
+    getJsonFile: function () {
+      var deferred = $q.defer();
+      window.requestFileSystem(LocalFileSystem.PERSISTENT,0,function(fileSystem) {
+        fileSystem.root.getDirectory('DrupalCon/', {create: true}, function (dirEntry) {
+          fileSystem.root.getDirectory('DrupalCon/json', {create: true}, function (dirUser) {
+              var movieURL = "http://dev.drupalcon.z9.dev.zyxware.net/sites/default/files/sessions.json";
+              var ft = new FileTransfer();
+              ft.download(movieURL, dirUser.toURL() + "/sessions.json", function(entry) {
+                deferred.resolve(entry);
+                console.log("inside services:" + entry.nativeURL);
+              }, function(err) { alert("Download failure: " + JSON.stringify(err)); });
+          }, function(err) { alert("2nd getDirectory failure: " + JSON.stringify(err)); });
+        }, function(err) { alert("1st getDirectory failure: " + JSON.stringify(err)); });
+      }, function(err) { alert("requestFileSystem failure: " + JSON.stringify(err)); });
+      return deferred.promise;
+    }
+  }
 });
